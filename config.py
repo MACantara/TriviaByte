543cd60d4d7a4e78e12a5ec0831aba5005b83859
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-01-11 12:42:47 +0800

    Remove unused environment variables from configuration

--- config.py
+++ config.py
@@ -6,10 +6,7 @@ load_dotenv()
 
 class Config:
     PRIMARY_GEMINI_API_KEY = os.getenv('PRIMARY_GEMINI_API_KEY')
-    SECONDARY_GEMINI_API_KEY = os.getenv('SECONDARY_GEMINI_API_KEY')
     DEBUG = True
     HOST = '0.0.0.0'
     PORT = 5000
     SECRET_KEY = os.getenv('SECRET_KEY')
-    GOOGLE_SEARCH_API_KEY = os.getenv('GOOGLE_SEARCH_API_KEY')
-    GOOGLE_SEARCH_ENGINE_ID = os.getenv('GOOGLE_SEARCH_ENGINE_ID')
