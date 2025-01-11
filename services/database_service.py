6c3ac6e515898576c0607546902378133c124405
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-01-11 15:34:25 +0800

    Add method to store a single question in the database

--- services/database_service.py
+++ services/database_service.py
@@ -4,6 +4,20 @@ import json
 
 class DatabaseService:
     @staticmethod
+    def store_single_question(question: str, options: List[str], correct_answer: str, topic: str):
+        """Store a single question in the database"""
+        db_question = Question(
+            question=question,
+            options=options,
+            correct_answer=correct_answer,
+            topic=topic
+        )
+        
+        db.session.add(db_question)
+        db.session.commit()
+        return db_question
+
+    @staticmethod
     def store_questions(questions: List[dict], topic: str):
         db_questions = []
         for q in questions:
