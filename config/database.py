ff2db8ffad56447ac6ffcb84c7f35f755294a874
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-01-11 15:06:00 +0800

    Refactor database setup to use Flask-SQLAlchemy and simplify initialization

--- config/database.py
+++ config/database.py
@@ -1,20 +1,9 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
-# Update URL to explicitly use postgresql dialect
-POSTGRES_URL = os.getenv('POSTGRES_URL_NON_POOLING')
-if POSTGRES_URL and not POSTGRES_URL.startswith('postgresql://'):
-    POSTGRES_URL = POSTGRES_URL.replace('postgres://', 'postgresql://')
+db = SQLAlchemy()
 
-engine = create_engine(POSTGRES_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
